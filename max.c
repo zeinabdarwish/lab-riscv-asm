dfad8ed21ed00f258fab62dc0f8d715c49302192
zeinabdarwish <48490074+zeinabdarwish@users.noreply.github.com>
2023-12-17 21:39:20 +0300

    Update max.c

--- max.c
+++ max.c
@@ -25,15 +25,15 @@ void findMaxColumnSum(int array[N][M]) {
     printf("The maximum column sum is %d, found in column index %d.\n", maxColumnSum, maxColumnIndex);
    
 }
-
+  
 int main() {
     int array[N][M] = {
-        {1, 2, 3, 4, 5, 6},
-        {7, 8, 9, 10, 11, 12},
-        {13, 14, 15, 16, 17, 18},
-        {19, 20, 21, 22, 23, 24},
-        {25, 26, 27, 28, 29, 30},
-        {31, 32, 33, 34, 35, 36}
+        {10, 2, 3, 4, 5, 6},
+        {7, 8, 9, 1, 1,  2},
+        {1, 1, 1, 1, 7,  8},
+        {9, 20, 2, 1, 3, 40},
+        {5, 6, 2, 8, 2,  3},
+        {1, 2, 3, 3, 3,  6}
     };
         findMaxColumnSum(array);
     return 0;
